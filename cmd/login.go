d4910c8f22aa7fc6292ad1e045ce89edce045815
Ngakan Gandhi <ngandhi@pmintl.net>
2026-05-29 14:37:11 +0700

    fix: treat expirations as milliseconds

--- cmd/login.go
+++ cmd/login.go
@@ -182,7 +182,7 @@ func writeAndFinish(
 
 	fmt.Printf("\n✓ Credentials written to ~/.aws/credentials [%s]\n", profileName)
 
-	expiry := time.Unix(creds.Expiration, 0)
+	expiry := time.UnixMilli(creds.Expiration, 0)
 	remaining := time.Until(expiry).Truncate(time.Minute)
 	fmt.Printf(" expires in %s\n", remaining)
 
